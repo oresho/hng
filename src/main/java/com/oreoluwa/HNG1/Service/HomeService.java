b864964b21159d573a26cdac345e1b58d9091fd7
oresho <somuyiwaoreoluwa@gmail.com>
2023-09-07 16:01:20 +0100

    cleaning code

--- src/main/java/com/oreoluwa/HNG1/Service/HomeService.java
+++ src/main/java/com/oreoluwa/HNG1/Service/HomeService.java
@@ -18,21 +18,27 @@ public class HomeService {
     private final String file_url = "https://github.com/oresho/hng/blob/stage1/src/main/java/com/oreoluwa/HNG1/Controller/HomeController.java";
     private final String repo_url = "https://github.com/oresho/hng";
     public ResponseModel process(String slack_name, String track){
-        Locale locale = new Locale("English");
-        LocalDate date = LocalDate.now();
-        DayOfWeek today = date.getDayOfWeek();
-        Instant now = Instant.now();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("uuuu-MM-dd'T'HH:mm:ss'Z'").withZone(ZoneOffset.UTC);
-        String formattedTimestamp = formatter.format(now);
         ResponseModel responseModel = ResponseModel.builder()
                 .slack_name(slack_name)
-                .current_day(today.getDisplayName(TextStyle.FULL_STANDALONE,locale))
-                .utc_time(formattedTimestamp)
+                .current_day(getDay())
+                .utc_time(getTimeUTC())
                 .track(track)
                 .github_file_url(file_url)
                 .github_repo_url(repo_url)
                 .status_code(HttpStatus.OK.value())
                 .build();
         return responseModel;
+    }
+
+    public String getDay(){
+        LocalDate date = LocalDate.now();
+        DayOfWeek today = date.getDayOfWeek();
+        return today.toString();
+    }
+
+    public String getTimeUTC(){
+        Instant now = Instant.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("uuuu-MM-dd'T'HH:mm:ss'Z'").withZone(ZoneOffset.UTC);
+        return formatter.format(now);
     }
 }
